36e3c2f3bff73b8a6dc9ce318888bd6c5192d6ad
Louis Royer <infos.louis.royer@gmail.com>
2024-12-03 18:07:25 +0100

    fix ohc desc

--- internal/app/pfcp.go
+++ internal/app/pfcp.go
@@ -116,7 +116,7 @@ func (p *PFCPServer) CreateSession(ue netip.Addr, uplinkTeid uint32, downlinkTei
 				ie.NewDestinationInterface(ie.DstInterfaceAccess),
 				ie.NewNetworkInstance(slice),
 				ie.NewOuterHeaderCreation(
-					0x01, // GTP/UDP/IPv4
+					0x0100, // GTP/UDP/IPv4
 					downlinkTeid,
 					gNB.String(),
 					"", 0, 0, 0,
